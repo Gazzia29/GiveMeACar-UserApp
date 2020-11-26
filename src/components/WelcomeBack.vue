456939bae8f418845079250f66efde11594eea74
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-11-26 13:48:55 +0100

    test

---
+++ src/components/WelcomeBack.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="flex-container">
+		<ion-item lines="none">
+			<ionAvatar slot="start">
+				<img src="../assets/avatar.jpg" />
+			</ionAvatar>
+			<ion-label color="white">
+				<p class="text text1">Vous êtes de retour</p>
+				<p class="text text2">Visiteur</p>
+			</ion-label>
+		</ion-item>
+	</div>
+</template>
+
+<script>
+	import {IonAvatar, IonItem, IonLabel} from "@ionic/vue";
+	import Compo from "./compo.vue";
+	import {defineComponent} from "vue";
+
+	export default defineComponent({
+		name: "WelcomeBack",
+		components: {IonAvatar, IonItem, IonLabel, Compo},
+	});
+</script>
+
+<style>
+	.flex-container {
+		display: flex;
+		gap: 20px;
+	}
+	.text {
+		font-weight: bold;
+		line-height: 1.1rem !important;
+	}
+	.text1 {
+		font-size: 12px !important;
+	}
+	.text2 {
+		font-size: 14px !important;
+	}
+</style>
---
+++ src/components/compo.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="blue">blfzefzea</div>
+</template>
+
+<script>
+	import {defineComponent} from "vue";
+	export default defineComponent({
+		name: "Compo",
+	});
+</script>
+
+<style scoped>
+	.blue {
+		color: blue;
+	}
+</style>
